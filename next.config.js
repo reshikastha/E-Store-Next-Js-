1fb1acac247b689a0019f5746c610d5cd5b53ded
Reshika Shrestha <shresthareshika@gmail.com>
2021-09-13 19:39:30 +0545

    mongodb connection

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-  env:{
-    "BASE_URL":"http://localhost:3000",
-    "MONGODB_URL":"mongodb+srv://e-Store:shrestha1@cluster0.0aafw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  }
-}
+// module.exports = {
+//   env:{
+//     "BASE_URL":"http://localhost:3000",
+//     "MONGODB_URL":"mongodb+srv://e-Store:shrestha1@cluster0.0aafw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+//   }
+// }
